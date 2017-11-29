cd6dbac177cd337165c1e79171f17aa3fe57df80
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2017-11-30 06:59:46 +1300

    Nothing, trigger heroku with debug_collectstatic

--- mofdb/settings.py
+++ mofdb/settings.py
@@ -18,6 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
