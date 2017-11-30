4b92edb011bad5fa8c3dd87c38cc148250042916
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2017-11-30 14:16:48 +1300

    Remove logging

--- mofdb/settings.py
+++ mofdb/settings.py
@@ -240,40 +240,40 @@ GRAPPELLI_AUTOCOMPLETE_SEARCH_FIELDS = {
 
 # settings.py
 ## LOG everything to console and to file (works in debug or in release)
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-            'datefmt' : "%d/%b/%Y %H:%M:%S"
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        },
-    },
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': 'mofdb.log',
-            'formatter': 'verbose'
-        },
-       'console': {
-           'level': 'DEBUG',
-           'class': 'logging.StreamHandler',
-           'formatter': 'verbose'
-      }
-    },
-    'loggers': {
-        'django': {
-            'handlers':['file', 'console'],
-            'propagate': True,
-            'level':'DEBUG',
-        },
-        'db': {
-            'handlers': ['file', 'console'],
-            'level': 'DEBUG',
-        },
-    }
-}
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'formatters': {
+#         'verbose': {
+#             'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+#             'datefmt' : "%d/%b/%Y %H:%M:%S"
+#         },
+#         'simple': {
+#             'format': '%(levelname)s %(message)s'
+#         },
+#     },
+#     'handlers': {
+#         'file': {
+#             'level': 'DEBUG',
+#             'class': 'logging.FileHandler',
+#             'filename': 'mofdb.log',
+#             'formatter': 'verbose'
+#         },
+#        'console': {
+#            'level': 'DEBUG',
+#            'class': 'logging.StreamHandler',
+#            'formatter': 'verbose'
+#       }
+#     },
+#     'loggers': {
+#         'django': {
+#             'handlers':['file', 'console'],
+#             'propagate': True,
+#             'level':'DEBUG',
+#         },
+#         'db': {
+#             'handlers': ['file', 'console'],
+#             'level': 'DEBUG',
+#         },
+#     }
+# }
